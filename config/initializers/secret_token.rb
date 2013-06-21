6d13abf5d583faec9ca613099874d3023733c548
subramaniand <subramaniand@aditi.com>
2013-06-21 17:35:09 +0000

    initial commit on azure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DiwaBlog::Application.config.secret_key_base = 'c2f48256f767f33fc815521f580c974431b79b56a8e1a8208a3f3da764edcc205fcc3a908e614f4ca8182d0905257494c80ea8f15ac73037e4aa9ecc10dd441d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DiwaBlog::Application.config.session_store :cookie_store, key: '_diwa_blog_session'
